43c42f4c36f22325d813e0fc94fb4845d0674ec6
Alan-imani <alanimani534@gmail.com>
2025-06-06 14:38:46 +0100

    Update scrape_reddit.js

--- netlify/functions/scrape_reddit.js
+++ netlify/functions/scrape_reddit.js
@@ -1,6 +1,15 @@
-export async function handler(event, context) {
+exports.handler = async function () {
+  const data = [
+    "Top-voted Digital Art Posts",
+    "Requests for New Art Styles",
+    "Trending Subreddits (r/digitalart, r/ArtCrit)"
+  ];
+
   return {
     statusCode: 200,
     body: JSON.stringify({
       platform: "reddit",
-      data: ["Subreddit: r/DigitalArt", "Popular Threads
+      data
+    })
+  };
+};
