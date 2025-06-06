5192b6aa23714ea0ceeff8bf071ea2e05d98de36
Alan-imani <alanimani534@gmail.com>
2025-06-06 14:26:38 +0100

    Update scrape_reddit.js

--- netlify/functions/scrape_reddit.js
+++ netlify/functions/scrape_reddit.js
@@ -1,7 +1,6 @@
-// Netlify Function: scrape_reddit
-exports.handler = async function(event, context) {
+export async function handler(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ platform: "reddit", data: "sample data" })
-  };
-};
+    body: JSON.stringify({
+      platform: "reddit",
+      data: ["Subreddit: r/DigitalArt", "Popular Threads
