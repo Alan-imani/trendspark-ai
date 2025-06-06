5266e9b4a16ffc7a1b3defda7fb108b7f623b1a3
Alan-imani <alanimani534@gmail.com>
2025-06-06 21:59:09 +0100

    Update scrape_etsy.js

--- netlify/functions/scrape_etsy.js
+++ netlify/functions/scrape_etsy.js
@@ -1,4 +1,4 @@
-export async function handler(event, context) {
+exports.handler = async function (event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -6,4 +6,4 @@ export async function handler(event, context) {
       data: ["Digital Art Bundle", "Watercolor Clipart", "SVG Logo Kit"]
     }),
   };
-}
+};
